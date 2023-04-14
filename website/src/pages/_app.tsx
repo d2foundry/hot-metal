23c62ace13cb032eaed7aadd98f18c1793139ac3
Katherine <k@gothfem.me>
2023-04-14 13:22:39 -0400

    feat: pull from schema on website

--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 import { Inter } from "next/font/google";
+import { ChakraProvider } from "@chakra-ui/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
