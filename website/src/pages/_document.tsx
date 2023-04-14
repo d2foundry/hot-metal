a2f649a47c869caeab8c05eebed00c5bed72249f
Kat Michaela <42180996+gothfemme@users.noreply.github.com>
2023-04-14 13:25:58 -0400

    feat: pull from schema on website (#4)

--- website/src/pages/_document.tsx
+++ website/src/pages/_document.tsx
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+          crossOrigin="anonymous"
+        ></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,9 +1,102 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
-// import validator from "@rjsf/validator-ajv8";
-// import Form from "@rjsf/core";
+
+import useSWR from "swr";
+import { UiSchema } from "@rjsf/utils";
+import validator from "@rjsf/validator-ajv8";
+import Form from "@rjsf/core";
+
+const GITHUB_JSON_SCHEMA_URI =
+  "https://raw.githubusercontent.com/d2foundry/hot-metal/main/data/schemas/source_schema.json";
+
+// const data = {
+//   $schema: "http://json-schema.org/draft-07/schema#",
+//   title: "Activities",
+//   type: "array",
+//   items: {
+//     $ref: "#/definitions/activity",
+//   },
+//   additionalItems: {
+//     type: "boolean",
+//   },
+//   definitions: {
+//     activity: {
+//       title: "Activity",
+//       type: "object",
+//       description:
+//         "Top-level activity from which there can be multiple loot sources.",
+//       properties: {
+//         name: {
+//           title: "Activity Name",
+//           type: "string",
+//         },
+//         description: {
+//           title: "Activity Description",
+//           type: "string",
+//         },
+//         rotationStartDate: {
+//           title: "Rotation Start Date",
+//           type: "string",
+//           description:
+//             "If this activity has sources that rotate, the date at reset where the rotation starts.",
+//           format: "date-time",
+//         },
+//         rotationDuration: {
+//           title: "Rotation Duration (ms)",
+//           type: "integer",
+//           description: "The time between each rotation change in millisconds",
+//         },
+//         lootSources: {
+//           title: "Loot Sources",
+//           type: "array",
+//           items: {
+//             $ref: "#/definitions/lootSource",
+//           },
+//         },
+//       },
+//       required: ["description", "name"],
+//     },
+//     lootSource: {
+//       type: "object",
+//       title: "Loot Source",
+//       description:
+//         "An encounter, rotator instance, etc from which a group of items can drop.",
+//       properties: {
+//         name: {
+//           title: "Source Name",
+//           type: "string",
+//         },
+//         description: {
+//           title: "Source Description",
+//           type: "string",
+//         },
+//         lootItems: {
+//           title: "Loot Items",
+//           type: "array",
+//           items: {
+//             description: "Bungie InventoryItem hash for the loot item",
+//             type: "integer",
+//           },
+//         },
+//       },
+//       required: ["description", "name", "lootItems"],
+//     },
+//     activityType: {
+//       type: "string",
+//       enum: ["raid", "dungeon"],
+//     },
+//   },
+// };
+const uiSchema: UiSchema = {
+  // activities: {
+  //   "ui:widget": "select", // could also be "select"
+  // },
+};
+
+const fetcher = (url: string) => fetch(url).then((r) => r.json());
 
 export default function Home() {
+  const { data, error, isLoading } = useSWR(GITHUB_JSON_SCHEMA_URI, fetcher);
   return (
     <>
       <Head>
@@ -14,7 +107,11 @@ export default function Home() {
       </Head>
       <div className={styles.main}>
         <h1>Hot Metal</h1>
-        <div></div>
+        <div>
+          {data ? (
+            <Form schema={data} validator={validator} uiSchema={uiSchema} />
+          ) : null}
+        </div>
       </div>
     </>
   );
